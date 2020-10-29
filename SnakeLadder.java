a3dd31375eb669fb47a3842fc143baceb4c61b14
Sagar <sagarrashinkar2@gmail.com>
2020-10-29 17:26:06 +0530

    ADDED: check option for no play, ladder and snake

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,13 +1,46 @@
+
+
 class SnakeLadder {
 
-	public static int startPos = 0;
-	public static int player = 1;
+	public static int playerPos = 0;
 	public static int winningPoints = 100;
+
+	public static int rollDice() {
+
+		int roll = (int)Math.floor(Math.random()*10)%6+1;
+		return roll;
+	}
+
+	public static int checkOption() {
+
+		return (int) Math.floor(Math.random()*10)%3 +1;
+	}
 
 	public static void main(String[] args) {
 
-		int roll = (int)Math.floor(Math.random()*10)%6+1;
+		int value = rollDice();
+		int option = checkOption();
 
-		System.out.println("Dies value is: "+roll);
+		switch(option) {
+
+			case 1:
+				System.out.println("There are no play!!!");
+				break;
+			case 2:
+				System.out.println("You got a ladder");
+				playerPos += value;
+				System.out.println("current position is:"+playerPos);
+				break;
+			case 3:
+				System.out.println("You got a snake");
+				playerPos -= value;
+				if(playerPos<0){
+
+					playerPos = 0;
+				}
+				System.out.println("current position is:"+playerPos);
+				break;
+
+		}
 	}
 }
