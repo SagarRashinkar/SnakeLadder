3012b312cd193753cf18694e6cf7b5c41a4db9fe
Sagar <sagarrashinkar2@gmail.com>
2020-10-29 16:33:41 +0530

    ADDED: game starts from position zero

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,8 +1,11 @@
 class SnakeLadder {
 
+	public static int startPos = 0;
+	public static int player = 1;
+	public static int winningPoints = 100;
+
 	public static void main(String[] args) {
 
-		System.out.println("welcome");
 
 	}
 }
